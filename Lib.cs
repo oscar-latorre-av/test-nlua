c80e3c3a49fc6b7a5fddd9b88979dadb4e744cb9
0xA74137 <A74137@pm.me>
2022-10-04 21:31:48 +0200

    Initial commit

---
+++ Lib.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_nlua
+{
+   public static class Lib
+   {
+      public static void CustomPrint(params object[] args)
+      {
+         Console.Write("CustomPrint: ");
+         Console.WriteLine("[" + string.Join(", ", args.Select(x => x.ToString())) + "]");
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using NLua;
+using test_nlua;
+
+var vm = new Lua();
+
+
+// Dump globals table
+//Console.WriteLine("Globals:");
+//try
+//{
+//   vm.DoString(@"
+
+//      for k, v in pairs(_G) do
+//         print(k, v)
+//      end
+      
+//   ");
+//}
+//catch { }
+//Console.WriteLine("------");
+
+
+// Disable "dangerous" modules
+vm.DoString(@"
+dofile = nil
+import = nil
+io = nil
+load = nil
+loadfile = nil
+luanet = nil
+os = nil
+package = nil
+xpcall = nil
+");
+
+
+
+// Custom thingy
+vm["xxx"] = (int a) => a * 2;
+vm["print"] = Lib.CustomPrint;
+vm.DoString(@"print('xxx =>', xxx(xxx(xxx(2))))");
+
+
+vm["disponibles"] = 168;
+vm["efectivas"] = 160.0;
+vm["extras"] = 7.2;
+vm["pausa"] = 1.4;
+
+Console.WriteLine($"disponibles => {vm["disponibles"]}");
+Console.WriteLine($"efectivas   => {vm["efectivas"]}");
+Console.WriteLine($"extras      => {vm["extras"]}");
+Console.WriteLine($"pausa       => {vm["pausa"]}");
+
+Console.WriteLine("------");
+
+vm.DoString(@"
+
+   efectivas = 168
+   extras = 0
+   pausa = 0.6
+
+");
+
+Console.WriteLine($"disponibles => {vm["disponibles"]}");
+Console.WriteLine($"efectivas   => {vm["efectivas"]}");
+Console.WriteLine($"extras      => {vm["extras"]}");
+Console.WriteLine($"pausa       => {vm["pausa"]}");
+
+
+
+// Try unsafe code
+try
+{
+   vm.DoString(@"
+
+      local file = io.open(""test.lua"", ""a+"")
+      io.output(file)
+      io.write(""HACKD!"");
+      
+   ");
+}
+catch
+{
+   Console.WriteLine("Hacking thwarted :)");
+}
+
